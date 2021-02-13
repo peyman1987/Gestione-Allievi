41612cf7dddc34ad1dcac46fc0efd5599e735cf6
peyman_em <peyman_em@yahoo.com>
2021-02-13 20:15:22 +0100

    add project

---
+++ src/main/java/com/peyman/gestione_allievi/GestioneAllieviApplication.java
@@ -0,0 +1,13 @@
+package com.peyman.gestione_allievi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneAllieviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneAllieviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peyman/gestione_allievi/GestioneAllieviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peyman.gestione_allievi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneAllieviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
